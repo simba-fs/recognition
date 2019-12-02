9f6f05d253f09904ca605001b1ab86fa6d9deca8
simba <simba.fs@gmail.com>
2019-12-02 22:10:32 +0800

    finish all

--- app.js
+++ app.js
@@ -7,26 +7,32 @@ if(!SpeechRecognition){
 }
 const recognition = new SpeechRecognition();
 
-const res = [{
-		keywords: 'how are you'.split(' '),
-		res: ['I\'am fine.', 'Not bad']
-	},{
-		keywords: ['good morning'],
-		res: ['Good morning']
-	},{
-		keywords: ['good night'],
-		res: ['good night', 'Have a good dream']
-	},{
-		keywords: 'ok google',
-		res: ['I live in google chrome, but I am not google assistant', 'does google assistant looks like me?', 'my pleasure']
-	},{
-		keywords: 'what is your age',
-		res: ['In fact, I\'m born last sunday', 'I cannot tell you']
-	},{
-		keywords: 'hello',
-		res: ['hello, what can I help you?']
+// reaction
+const response = [];
+function addReaction(req, res){
+	var index = response.findIndex((item)=>item.keywords === req);
+	
+	if(index === -1){
+		//add a new reaction
+		response.push({
+			keywords: req,
+			res: [res]
+		});
+	}else{
+		// update an old one
+		response[index].res.push(res);
 	}
-]
+}
+
+// add some data to response
+addReaction('hello', 'hello');
+addReaction('what\'s your age', 'five');
+addReaction('ok google', 'I live in google chrome, but I\'m not google assistant');
+addReaction('ok google', 'does google assistant looks like me?');
+addReaction('how are you', 'I\'m fine');
+addReaction('how are you', 'Not bad');
+addReaction('good night', 'Good noght');
+addReaction('good night', 'Have a nice dream');
 
 recognition.lang = 'en-US';
 
@@ -38,7 +44,7 @@ recognition.onresult = (event) => {
 	console.log(event);
 	const result = event.results[0][0].transcript;
 	console.log(result);
-	let text = response(result);
+	let text = getResponse(result);
 	content.append('<p>you said: ' + result + '</P>' );
 	content.append('<p>this page said: ' + text + '</p>')
 	readOutLoud(text);
@@ -48,22 +54,34 @@ start.click(()=>{
 	recognition.start();
 });
 
-function response(msg){
+function getResponse(msg){
+	msg = msg.toLowerCase();
 	rate = [];
-	for(let i in res){
+	for(let i in response){
 		let count = 0;
 		
-		let that = res[i].keywords;
+		let that = response[i].keywords.split(' ');
 		for(let j in that){
+			console.log('msg', msg, that[j]);
 			if(msg.includes(that[j])){
 				count ++;
+				console.log('normal', that, '++');
+			}
+			let position = Math.abs(((j-0+1)/(that.length+1)) - ((msg.indexOf(that[j])+1)/(msg.length+1)));
+			console.log('position', position);
+			if(position < 0.1){
+				count ++;
+				console.log('position', that, '++');
 			}
 		}
+		console.log(that, count + msg.length - 1, response[i].keywords.length);
 		if(count){
-			rate.push(count/res[i].keywords.length);
+			rate.push((count + msg.length - 1)/response[i].keywords.length);
 		}else{
 			rate.push(0);
 		}
+
+		console.log('============');
 	}
 	console.log(rate);
 	console.log(rate.filter(function(item){return item >= 0.5}).length);
@@ -78,7 +96,7 @@ function response(msg){
 			max = i;
 		}
 	}
-	let res_ = res[max].res;
+	let res_ = response[max].res;
 	let finalIndex = Math.random() * res_.length;
 	return res_[Math.floor(finalIndex)];
 }
